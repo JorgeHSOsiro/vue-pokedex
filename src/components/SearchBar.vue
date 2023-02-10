58853bf4bcb1345fb52b0495cebcd24f4e68bea8
JorgeHSOsiro <jojimurakabe@gmail.com>
2023-02-10 11:54:24 -0300

    feat: add searchbar component
    adds searchbar component, install font awesome for icons, remove unused files

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="form" role="form" aria-label="Barra de busca de pokémon">
+    <input type="text" placeholder=" ex.: Pikachu" />
+    <button>
+      <span class="icon">
+        <i class="fas fa-search"></i>
+      </span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SearchBar",
+});
+</script>
+
+<style lang="scss">
+.form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  input {
+    padding: 6px;
+    font-size: 17px;
+  }
+  input:focus {
+    outline: none;
+  }
+  button {
+    padding: 6px 10px;
+    font-size: 21px;
+    border: none;
+    background-color: #3EA6F0;
+    color: white;
+    cursor: pointer;
+  }
+  button:hover {
+    opacity: 0.8;
+  }
+}
+</style>
